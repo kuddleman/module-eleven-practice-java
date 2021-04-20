dcec1e6c8baf4c84150df4bf89d47b34e27a66e4
Kuddleman <edl1155@gmail.com>
2021-04-20 10:43:38 -0700

    convert list to stream

--- src/StreamPractice.java
+++ src/StreamPractice.java
@@ -6,8 +6,27 @@ public class StreamPractice {
 
     public static void main(String[] args) {
         List<Integer> numberList = fillNumberList(10000, 1000);
-    }
 
+
+    // count the number of times a target appears - iteration versus streams
+
+        int target = 43;
+        long count = 0;
+
+        for(int number : numberList){
+            if(number == target) {
+                count++;
+            }
+        }
+        System.out.println(target + " appears in the list " + count + " times.");
+
+        // stream approach
+
+        //convert list to stream:
+        count = numberList.stream().filter((number) -> number == target).count();
+        System.out.println(target + " appears in the list " + count + " times.");
+
+    }
     private static List<Integer> fillNumberList(int size, int max) {
         Random generator = new Random();
         List<Integer> numbers = new ArrayList<>();
