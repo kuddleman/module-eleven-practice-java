45e2e78561ef98856af64dad4018742ba8cbf817
Kuddleman <edl1155@gmail.com>
2021-04-20 20:40:31 -0700

    add streams

--- src/StreamPractice.java
+++ src/StreamPractice.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class StreamPractice {
 
     public static void main(String[] args) {
+
+        /*
         List<Integer> numberList = fillNumberList(10000, 1000);
 
 
@@ -25,8 +28,28 @@ public class StreamPractice {
         //convert list to stream:
         count = numberList.stream().filter((number) -> number == target).count();
         System.out.println(target + " appears in the list " + count + " times.");
+ */
+
+        // create some streams!
+        int printNum = 10;
+        int randomNumberLimit = 1000;
+        Random generator = new Random();
+        Stream<Integer> randomIntStream = Stream.generate(
+                () -> generator.nextInt(randomNumberLimit)
+        );
+        randomIntStream.limit(printNum).forEach(System.out::println);
+
+        // infinite stream of odd positive numbers:
+        Stream.iterate(1, num -> num + 2).limit(printNum).forEach(System.out::println);
+
+
+
+
+
 
     }
+
+
     private static List<Integer> fillNumberList(int size, int max) {
         Random generator = new Random();
         List<Integer> numbers = new ArrayList<>();
@@ -35,4 +58,12 @@ public class StreamPractice {
         }
         return numbers;
     }
+
+
+
+
+
+
 }
+
+
